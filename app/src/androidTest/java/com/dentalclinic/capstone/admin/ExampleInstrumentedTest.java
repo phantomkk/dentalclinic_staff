86258fb258a320de807ce92883acd89e2b2a9bf9
lucns <luckyfire1996@gmail.com>
2018-07-09 12:52:53 +0700

    LucNS:add gradle firebase

--- app/src/androidTest/java/com/dentalclinic/capstone/admin/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dentalclinic/capstone/admin/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.lenovo.dentalclinicadmin", appContext.getPackageName());
+        assertEquals("com.dentalclinic.capstone.admin", appContext.getPackageName());
     }
 }
