e646a24761da4bcf84cfd5a8f51e7a39191f632d
lucns <luckyfire1996@gmail.com>
2018-07-24 15:09:14 +0700

    LucNS: fix update payment price

--- app/src/main/java/com/dentalclinic/capstone/admin/api/services/PatientService.java
+++ app/src/main/java/com/dentalclinic/capstone/admin/api/services/PatientService.java
@@ -1,6 +1,7 @@
 package com.dentalclinic.capstone.admin.api.services;
 
 import com.dentalclinic.capstone.admin.api.requestobject.PatientProfileRequest;
+import com.dentalclinic.capstone.admin.api.responseobject.SuccessResponse;
 import com.dentalclinic.capstone.admin.models.Patient;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 import io.reactivex.Single;
 import retrofit2.Response;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -16,4 +18,7 @@ import retrofit2.http.Query;
 public interface PatientService {
     @GET("api/patient/getByPhone")
     Single<Response<List<Patient>>> getPatientsByPhone(@Query("phone") String phone);
+
+    @POST("api/patient/receive")
+    Single<Response<SuccessResponse>> beginTreatment(@Field("patient_id") int patientId);
 }
